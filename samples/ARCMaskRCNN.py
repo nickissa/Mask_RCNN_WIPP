7b3509321d41f950d1d0363558e217f93bf09191
Nicolas Issa <nicolasissambsh@gmail.com>
2023-04-24 16:08:37 -0400

    Added base code in python and jupyter notebook

---
+++ samples/ARCMaskRCNN.py
@@ -0,0 +1,168 @@
+import os
+import sys
+import random
+import math
+import numpy as np
+import skimage.io
+import matplotlib
+import matplotlib.pyplot as plt
+import cv2
+import time
+
+# Root directory of the project
+ROOT_DIR = os.path.abspath("../")
+print("Root Dir:", ROOT_DIR)
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+
+from mrcnn import utils
+import mrcnn.model as modellib
+from mrcnn import visualize
+
+# Import COCO config
+sys.path.append(os.path.join(ROOT_DIR, "samples/coco/"))  # To find local version
+import coco
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Local path to trained weights file
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+
+# Download COCO trained weights from Releases if needed
+if not os.path.exists(COCO_MODEL_PATH):
+    utils.download_trained_weights(COCO_MODEL_PATH)
+
+
+class InferenceConfig(coco.CocoConfig):
+    # Set batch size to 1 since we'll be running inference on
+    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+    NUM_CLASSES = 1 + 45
+
+config = InferenceConfig()
+config.display()
+
+# Create model object in inference mode.
+model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+
+# Load weights trained on MS-COCO
+#model.load_weights(COCO_MODEL_PATH, by_name=True)
+model.load_weights(COCO_MODEL_PATH, by_name=True, exclude=["mrcnn_class_logits", "mrcnn_bbox_fc", "mrcnn_bbox", "mrcnn_mask"])
+
+# COCO Class names
+# Index of the class in the list is its ID. For example, to get ID of
+# the teddy bear class, use: class_names.index('teddy bear')
+class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+               'bus', 'train', 'truck', 'boat', 'traffic light',
+               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+               'kite', 'baseball bat', 'baseball glove', 'skateboard',
+               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+               'teddy bear', 'hair drier', 'toothbrush']
+
+def random_colors(N):
+    np.random.seed(1)
+    colors = [tuple(255 * np.random.rand(3)) for _ in range(N)]
+    return colors
+
+
+def apply_mask(image, mask, color, alpha=0.5):
+    """apply mask to image"""
+    for n, c in enumerate(color):
+        image[:, :, n] = np.where(
+            mask == 1,
+            image[:, :, n] * (1 - alpha) + alpha * c,
+            image[:, :, n]
+        )
+    return image
+
+def display_instances(image, boxes, masks, ids, names, scores):
+    """
+        take the image and results and apply the mask, box, and Label
+    """
+    n_instances = boxes.shape[0]
+
+    if not n_instances:
+        print('NO INSTANCES TO DISPLAY')
+    else:
+        assert boxes.shape[0] == masks.shape[-1] == ids.shape[0]
+
+    for i in range(n_instances):
+        if not np.any(boxes[i]):
+            continue
+
+        y1, x1, y2, x2 = boxes[i]
+        label = names[ids[i]]
+        color = class_dict[label]
+        score = scores[i] if scores is not None else None
+        caption = '{} {:.2f}'.format(label, score) if score else label
+        mask = masks[:, :, i]
+
+        image = apply_mask(image, mask, color)
+        image = cv2.rectangle(image, (x1, y1), (x2, y2), color, 2)
+        image = cv2.putText(
+            image, caption, (x1, y1), cv2.FONT_HERSHEY_COMPLEX, 0.7, color, 2
+        )
+
+    return image
+
+# Define a video capture object
+vid = cv2.VideoCapture(0)              # real-time webcam
+#vid = cv2.VideoCapture('videofile2.mp4') # pre-recorded video
+
+size = (
+    int(vid.get(cv2.CAP_PROP_FRAME_WIDTH)),
+    int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+)
+
+codec = cv2.VideoWriter_fourcc(*'DIVX')
+output = cv2.VideoWriter('testfile3_masked_30fps.avi', codec, 30.0, size)
+
+# these 2 lines can be removed if you dont have a 1080p camera.
+#vid.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+#vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+colors = random_colors(len(class_names))
+class_dict = {
+    name: color for name, color in zip(class_names, colors)
+}
+
+starttime = time.time()
+
+while(True):
+    ret, frame = vid.read()
+    
+    if ret == False:
+        break
+        
+    # Run detection
+    results = model.detect([frame], verbose=1)
+    
+    # Visualize results
+    r = results[0]
+    frame = display_instances(frame, r['rois'], r['masks'], r['class_ids'], class_names, r['scores'])
+    
+    output.write(frame)
+    cv2.imshow('Frame', frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+vid.release()
+output.release()
+cv2.destroyAllWindows()
+
+endtime = time.time()
+totaltime = endtime - starttime
+print('Total Time Taken:', totaltime/60, 'min(s)')
